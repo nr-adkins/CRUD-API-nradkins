f6bf0a87d4446ec855c213ad7d3f232790488fcf
nadkins <nradkins@uncg.edu>
2026-03-06 11:18:13 -0500

    character.java file

--- src/main/java/character.java
+++ src/main/java/character.java
@@ -27,6 +27,53 @@ public class Character{
         this.universe = universe;
         this.age = age;
     }
+    public Long getCharacterId() {
+        return characterId;
+    }
 
-    
+    public void setCharacterId(Long characterId) {
+        this.characterId = characterId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getUniverse() {
+        return universe;
+    }
+
+    public void setUniverse(String universe) {
+        this.universe = universe;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
 }
