0de22d349816588268bda6ae0df05e75da0a9a76
nadkins <nradkins@uncg.edu>
2026-03-06 12:13:27 -0500

    build

--- src/main/java/com/example/demo/CharacterRepository.java
+++ src/main/java/com/example/demo/CharacterRepository.java
@@ -4,7 +4,7 @@ import com.example.demo.entity.Charcter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.list;
 
-public interface CharacterRepository extends JpaRepositroy<Character, Long>{
+public interface CharacterRepository extends JpaRepository<Character, Long>{
     List<Character> findByUniverse( String universe);
     List<Character> findByNameContains(String name);
     
